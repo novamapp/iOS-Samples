539717762efae0d53339aba5532c2f3a034eb3ba
novamapp <ogheneovoabu@gmail.com>
2022-02-08 02:27:35 -0600

    base of project002

---
+++ Project002/Project002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project002
+//
+//  Created by Ogheneovo Abu on 2/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
